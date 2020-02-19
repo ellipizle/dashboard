72026fa0b399c7763799f4638d7e203532138f57
ellipizle <blackshifu@localhost.localdomain>
2020-02-19 02:25:59 +0100

    increase step

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -40,15 +40,15 @@ export class HeaderComponent implements OnInit {
 		{ label: '1d', value: 86400000 }
 	];
 	rangeList = [
-		{ label: 'Last 5 minutes', short: '5m', start: 0, end: 0 },
-		{ label: 'Last 15 minutes', short: '15m', start: 0, end: 0 },
-		{ label: 'Last 30 minutes', short: '30m', start: 0, end: 0 },
-		{ label: 'Last 1 hour', short: '1h', start: 0, end: 0 },
-		{ label: 'Last 3 hours', short: '3h', start: 0, end: 0 },
-		{ label: 'Last 6 hours', short: '6h', start: 0, end: 0 },
-		{ label: 'Last 12 hours', short: '12h', start: 0, end: 0 },
-		{ label: 'Last 24 hours', short: '24h', start: 0, end: 0 },
-		{ label: 'Last 2 days', short: '2d', start: 0, end: 0 }
+		{ label: 'Last 5 minutes', short: '5m', start: 0, end: 0, step: 5 },
+		{ label: 'Last 15 minutes', short: '15m', start: 0, end: 0, step: 15 },
+		{ label: 'Last 30 minutes', short: '30m', start: 0, end: 0, step: 30 },
+		{ label: 'Last 1 hour', short: '1h', start: 0, end: 0, step: 60 },
+		{ label: 'Last 3 hours', short: '3h', start: 0, end: 0, step: 180 },
+		{ label: 'Last 6 hours', short: '6h', start: 0, end: 0, step: 360 },
+		{ label: 'Last 12 hours', short: '12h', start: 0, end: 0, step: 720 },
+		{ label: 'Last 24 hours', short: '24h', start: 0, end: 0, step: 1440 },
+		{ label: 'Last 2 days', short: '2d', start: 0, end: 0, step: 2880 }
 	];
 	public setRange(range) {
 		switch (range.short) {
